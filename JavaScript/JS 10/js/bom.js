49c07751741cca98f334b9302b7c50949a980318
muradhaji <muradhajiyev42@icloud.com>
2024-03-16 20:02:39 +0400

    dom explained

---
+++ JavaScript/JS 10/js/bom.js
@@ -0,0 +1,70 @@
+// function testFunc() {
+//   console.log(this);
+// }
+
+// x = 5;
+
+// let testObj = {
+//   testMethod: testFunc,
+//   // testMethod: function () {
+//   //   console.log(this);
+//   // },
+// };
+
+// testFunc();
+// window.testFunc();
+// console.log(window.x);
+
+// testObj.testMethod();
+
+// console.log(this);
+// console.log(window);
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+// console.log(window.outerWidth);
+// console.log(window.outerHeight);
+// console.log(window.scrollY);
+
+// window.addEventListener('scroll', () => {
+//   console.log(window.scrollY);
+//   if (window.scrollY >= 200) {
+//     document.body.style.backgroundColor = 'black';
+//     document.body.style.color = 'white';
+//   } else {
+//     document.body.style.backgroundColor = 'white';
+//     document.body.style.color = 'black';
+//   }
+// });
+
+// window.alert();
+// window.prompt();
+// window.confirm();
+
+// console.log(navigator);
+// console.log(navigator.platform);
+// console.log(navigator.userAgent);
+
+// navigator.geolocation.watchPosition(
+//   (x) => {
+//     console.log(x.coords.latitude, x.coords.longitude);
+//   },
+//   (y) => {
+//     console.log(y.message);
+//   }
+// );
+
+// console.log(screen);
+// console.log(screen.orientation.angle);
+// console.log(screen.orientation.type);
+
+// location.reload();
+// location.replace("https://www.azza.az");
+
+// console.log(history.length);
+// history.back();
+// history.forward();
+// history.go(-4);
+// history.go(3);
+
+// window.open('https://www.azza.az');
+// window.close();
---
+++ JavaScript/JS 10/js/dom.js
@@ -0,0 +1,82 @@
+// console.log(document);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// document.body.style.backgroundColor = 'black';
+// document.body.setAttribute("id", "asdfg");
+
+// function addClass() {
+//   document.body.setAttribute('class', 'menu');
+// }
+
+// function removeClass() {
+//   document.body.setAttribute('class', '');
+// }
+
+// function setRandomColor() {
+//   let r = Math.trunc(Math.random() * 256);
+//   let g = Math.trunc(Math.random() * 256);
+//   let b = Math.trunc(Math.random() * 256);
+//   document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+// }
+
+// console.log(document.body.childNodes[3].childNodes[3].childNodes[1]);
+// console.log(document.body.childNodes[3].childNodes[3].childNodes[1]);
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.body.parentNode);
+// console.log(document.head.parentNode);
+// console.log(document.head.parentNode == document.body.parentNode);
+
+// console.log(document.childNodes)
+
+// let testp1 = document.body.childNodes[5].nextSibling.nextSibling;
+// let testp2 = document.body.childNodes[9].previousSibling.previousSibling;
+
+// console.log(testp1);
+// console.log(testp2);
+// console.log(testp1 == testp2);
+
+// console.log(document.body.firstChild.previousSibling);
+// console.log(document.body.lastChild.nextSibling);
+
+// console.log(document.body.childNodes);
+// console.log(document.body.children);
+
+// console.log(document.body.parentNode);
+// console.log(document.body.parentElement);
+
+// console.log(document.body.children[0].children[1]);
+// console.log(document.body.children[0].children[1].previousSibling);
+// console.log(document.body.children[0].children[1].previousElementSibling);
+// console.log(document.body.children[0].children[1].nextSibling);
+// console.log(document.body.children[0].children[1].nextElementSibling);
+// console.log(
+//   document.body.children[0].children[1].nextElementSibling.nextSibling
+// );
+// console.log(
+//   document.body.children[0].children[1].nextElementSibling.nextElementSibling
+// );
+
+// console.log(document.documentElement.parentElement);
+// console.log(document.documentElement.parentNode);
+
+// console.log(document.body.children[0].children[0].children[1].children[0]);
+
+// console.log(document.getElementById('aboutbutton'));
+
+// let myheader = document.getElementById('myhead1');
+
+// myheader.innerText = '<p>Hello</p>';
+// myheader.innerHTML = '<p>Hello</p>';
+
+/*
+Task 
+
+let user = ["John Doe", "Mike Ross", "Elliot Alderson"];
+
+Random olaraq 3 ad seç və H1-lərin içərisinə yazdır, H1-ə dizayn ver mərkəzə gətir.
+*/
